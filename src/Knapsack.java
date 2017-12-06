f812bdd429fb8c511fd3a3d293201e738d23dfec
Yang <jiaoyang813@gmail.com>
2017-12-05 22:05:57 -0500

    Naive knapsack solution

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,6 +1,31 @@
 public class Knapsack {
     public static void main(String[] args)
     {
-        System.out.println("Test");
+        int[] val = {60, 100, 120};
+        int[] wt = {10,20,30};
+        int W = 50;
+
+       System.out.print("Naive Solution: " +  NaiveWay(val, wt , W, 3));
+    }
+
+    private static int Max(int a, int b)
+    {
+        if(a > b)
+            return a;
+        return b;
+    }
+
+    private static int NaiveWay(int[] val, int[] weight, int W, int i)
+    {
+        if(W==0 || i == 0)
+            return 0;
+
+        if(weight[i - 1] > W)
+            return NaiveWay(val, weight, W, i-1);
+        else
+        {
+            return Max(NaiveWay(val, weight, W, i-1),
+             val[i-1] + NaiveWay(val, weight, W-weight[i-1], i-1));
+        }
     }
 }
